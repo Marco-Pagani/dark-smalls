2a6e6a54b0c441e551f2c040066ae04509c57d3a
Marco Pagani <marco@pagani.io>
2023-09-25 21:45:48 -0400

    a game loop wow

--- sketch.js
+++ sketch.js
@@ -1,24 +1,25 @@
-const MOVES = ['ROCK', 'PAPER', 'SCISSORS']
+const MOVES = ['SCISSORS', 'ROCK', 'PAPER']
 const levels = [
-  { attacks: 1, successMargin: 0.3 },
-  { attacks: 1, successMargin: 0.3 },
-  { attacks: 1, successMargin: 0.3 },
-  { attacks: 2, successMargin: 0.25 },
-  { attacks: 2, successMargin: 0.25 },
-  { attacks: 2, successMargin: 0.25 },
-  { attacks: 3, successMargin: 0.2 },
-  { attacks: 3, successMargin: 0.15 },
-  { attacks: 4, successMargin: 0.15 },
-  { attacks: 5, successMargin: 0.1 },
+  { attacks: 1, successMargin: 0.3, enemyName: 'AAAA Battery, The Lesser' },
+  { attacks: 1, successMargin: 0.3, enemyName: '2' },
+  { attacks: 1, successMargin: 0.3, enemyName: '3' },
+  { attacks: 2, successMargin: 0.25, enemyName: '4' },
+  { attacks: 2, successMargin: 0.25, enemyName: '5' },
+  { attacks: 2, successMargin: 0.25, enemyName: '6' },
+  { attacks: 3, successMargin: 0.2, enemyName: '7' },
+  { attacks: 3, successMargin: 0.15, enemyName: '8' },
+  { attacks: 4, successMargin: 0.15, enemyName: '9' },
+  { attacks: 5, successMargin: 0.1, enemyName: '10' },
 ]
 
-let gameStatus = 1
+let gameStatus = 0
 
 const gameState = {
-  level: 0,
+  level: -1,
   health: 3
 }
 const levelState = {
+  startTime: null,
 
 }
 const roundState = {
@@ -40,7 +41,7 @@ function setup() {
 function draw() {
   background(240)
   switch (gameStatus) {
-    case 0: break
+    case 0: updateGameState(); break
     case 1: updateLevelState(); break
     case 2: updateRoundState(); break
     case 3: updateAttackState(); break
@@ -48,16 +49,41 @@ function draw() {
   drawDebug()
 }
 
-function updateLevelState() {
+function updateGameState() {
+  text('click to start', 100, 100)
   if (mouseIsPressed) { // init round
-    gameStatus = 2
-    roundState.startTime = millis()
-    roundState.enemyAttacks = new Array(levels[gameState.level]).fill(0).map(e => Math.floor(Math.random() * 3))
+    gameStatus = 1
+    levelState.startTime = millis()
   }
 }
 
-function updateRoundState() {
+function updateLevelState() {
+  const time = millis() - levelState.startTime
 
+  text(`Level ${gameState.level + 1}`, 100, 100)
+
+  if (time > 2000) {
+    text(levels[gameState.level + 1].enemyName, 200, 200)
+  }
+
+
+  if (time > 4000) {
+    gameStatus = 2
+    gameState.level = gameState.level + 1
+    roundState.startTime = millis()
+    roundState.enemyAttacks = new Array(levels[gameState.level].attacks).fill(0).map(e => Math.floor(Math.random() * 3))
+    roundState.currentAttack = 0
+  }
+
+}
+
+function updateRoundState() {
+  console.log(roundState.currentAttack, roundState.enemyAttacks.length)
+  if (roundState.currentAttack === roundState.enemyAttacks.length) {
+    gameStatus = 1
+    levelState.startTime = millis()
+    return
+  }
 
   const time = millis() - roundState.startTime
 
@@ -65,7 +91,7 @@ function updateRoundState() {
 
   if (time > 1000) drawAttackBar()
 
-  if (roundState.currentAttack === roundState.enemyAttacks.length) return
+
 
   if (time > 2000) {
     gameStatus = 3
@@ -87,10 +113,17 @@ function handleAttackInput() {
   if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) chosenMove = 2
 
   if (chosenMove >= 0) { // user made their move
-    const success = chosenMove === roundState.enemyAttacks[roundState.currentAttack]
-    console.log(success)
+    const successfulMove = chosenMove === roundState.enemyAttacks[roundState.currentAttack]
+    const successfulTiming = attackState.chargeProgress < 1 && attackState.chargeProgress > (1 - levels[gameState.level].successMargin)
+    if (!(successfulMove && successfulTiming)) {
+      gameState.health = gameState.health - 1
+      if (gameState.health === 0) {
+        // GAME OVER
+      }
+    }
     gameStatus = 2
     roundState.currentAttack = roundState.currentAttack + 1
+    attackState.chargeProgress = 0
   }
 }
 
